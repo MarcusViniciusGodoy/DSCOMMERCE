a349c08761536efaefcc8d4d41da1b0f121f1be2
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-03-04 17:49:53 -0300

    Criado o pacote DTO e pacote Services

--- src/main/java/com/desafio/dscommerce/controllers/ProductController.java
+++ src/main/java/com/desafio/dscommerce/controllers/ProductController.java
@@ -1,26 +1,25 @@
 package com.desafio.dscommerce.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.desafio.dscommerce.DTO.ProductDTO;
 import com.desafio.dscommerce.entities.Product;
 import com.desafio.dscommerce.repositories.ProductRepository;
+import com.desafio.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
     @Autowired
-    private ProductRepository repository;
+    private ProductService service;
 
-    @GetMapping
-    public String teste(){
-        Optional<Product> result = repository.findById(1L);
-        Product product = result.get();
-        return product.getName();
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        return service.findById(id);
     }   
 }
---
+++ src/main/java/com/desafio/dscommerce/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.desafio.dscommerce.services;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.desafio.dscommerce.DTO.ProductDTO;
+import com.desafio.dscommerce.entities.Product;
+import com.desafio.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id){
+        Optional<Product> result = repository.findById(id);
+        Product product = result.get();
+        ProductDTO dto = new ProductDTO(product);
+        return dto;
+    }
+}
