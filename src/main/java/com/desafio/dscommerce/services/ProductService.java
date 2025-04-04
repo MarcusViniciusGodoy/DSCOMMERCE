c38371eacc304e62ef0e9925b92e176032e20087
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-04 15:12:27 -0300

    Controle de acesso por perfil e rota

--- src/main/java/com/desafio/dscommerce/services/ProductService.java
+++ src/main/java/com/desafio/dscommerce/services/ProductService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.desafio.dscommerce.DTO.ProductDTO;
+import com.desafio.dscommerce.DTO.ProductMinDTO;
 import com.desafio.dscommerce.entities.Product;
 import com.desafio.dscommerce.repositories.ProductRepository;
 import com.desafio.dscommerce.services.exceptions.DatabaseException;
@@ -29,9 +30,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(String name, Pageable pageable){
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
         Page<Product> result = repository.searchByName(name, pageable);
-        return result.map(x -> new ProductDTO(x));
+        return result.map(x -> new ProductMinDTO(x));
     }
 
     @Transactional
