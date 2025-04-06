4fb54d8a456899a56ea0ab886f7160a8bc540ab3
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-04-06 16:11:55 -0300

    Busca categorias

---
+++ src/main/java/com/desafio/dscommerce/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.desafio.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.desafio.dscommerce.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
