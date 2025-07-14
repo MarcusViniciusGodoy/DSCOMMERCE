3819d5db7a0aea63896e866a4b8225a6b9413810
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-07-14 09:59:05 -0300

    Testes controle de acesso admin

---
+++ src/test/java/com/desafio/dscommerce/services/AuthServiceTests.java
@@ -0,0 +1,43 @@
+package com.desafio.dscommerce.services;
+
+import com.desafio.dscommerce.entities.User;
+import com.desafio.dscommerce.tests.UserFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class AuthServiceTests {
+
+    @InjectMocks
+    private AuthService service;
+
+    @Mock
+    private UserService userService;
+
+    private User admin, selfClient, otherClient;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        admin = UserFactory.createAdminUser();
+        selfClient = UserFactory.createCustomClientUser(1L, "Bob");
+        otherClient = UserFactory.createCustomClientUser(2L, "Ana");
+    }
+
+    @Test
+    public void validateSelfOrAdminShouldDoNothingWhenAdminLogged(){
+
+        Mockito.when(userService.authenticated()).thenReturn(admin);
+
+        Long userId = admin.getId();
+
+        Assertions.assertDoesNotThrow(() -> {
+            service.validateSelfOrAdmin(userId);
+        });
+    }
+}
