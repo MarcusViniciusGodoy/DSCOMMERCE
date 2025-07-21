500c070d757fd33f4112e4e03b295d362c87a9f6
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-07-21 08:43:02 -0300

    Teste OrderController

---
+++ src/test/java/com/desafio/dscommerce/controllers/it/OrderControllerIT.java
@@ -0,0 +1,89 @@
+package com.desafio.dscommerce.controllers.it;
+
+import com.desafio.dscommerce.DTO.OrderDTO;
+import com.desafio.dscommerce.DTO.ProductDTO;
+import com.desafio.dscommerce.entities.*;
+import com.desafio.dscommerce.tests.ProductFactory;
+import com.desafio.dscommerce.tests.TokenUtil;
+import com.desafio.dscommerce.tests.UserFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class OrderControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TokenUtil tokenUtil;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String clientUsername, clientePassword, adminUsername, adminPassword;
+    private String adminToken, clientToken, invalidToken;
+    private Long existingOrderId, nonExistingOrderId;
+
+    private Order order;
+    private OrderDTO orderDTO;
+    private User user;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        clientUsername = "maria@gmail.com";
+        clientePassword = "123456";
+        adminUsername = "alex@gmail.com";
+        adminPassword = "123456";
+
+        existingOrderId = 1L;
+        nonExistingOrderId = 100L;
+
+        adminToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+        clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientePassword);
+        invalidToken = adminToken + "xpto"; //simulates wrong password
+
+        user = UserFactory.createClientUser();
+        order = new Order(null, Instant.now(), OrderStatus.WAITING_PAYMENT, user, null);
+
+        Product product = ProductFactory.createProduct();
+        OrderItem orderItem = new OrderItem(order, product, 2, 10.0);
+        order.getItems().add(orderItem);
+    }
+
+    @Test
+    public void findByIdShouldReturnOrderDTOWhenIdExistsWhenAdminLogged() throws Exception{
+
+        ResultActions result =
+                mockMvc.perform(get("/orders/{id}", existingOrderId)
+                                .header("Authorization", "Bearer " + adminToken)
+                                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").value(existingOrderId));
+        result.andExpect(jsonPath("$.moment").value("2022-07-25T13:00:00Z"));
+        result.andExpect(jsonPath("$.status").value("PAID"));
+        result.andExpect(jsonPath("$.client").exists());
+        result.andExpect(jsonPath("$.payment").exists());
+        result.andExpect(jsonPath("$.items").exists());
+        result.andExpect(jsonPath("$.total").exists());
+    }
+}
