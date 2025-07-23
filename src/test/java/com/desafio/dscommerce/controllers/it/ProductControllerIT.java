a2ac697c1c0d22896c65381f3c27914c4933c20b
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-07-23 08:43:47 -0300

    Teste update price

--- src/test/java/com/desafio/dscommerce/controllers/it/ProductControllerIT.java
+++ src/test/java/com/desafio/dscommerce/controllers/it/ProductControllerIT.java
@@ -304,6 +304,41 @@ public class ProductControllerIT {
         result.andExpect(status().isUnprocessableEntity());
     }
 
+    @Test
+    public void updateShouldReturnUnprocessableEntityWhenIdExistsAndAdminLoggedAndPriceIsNegative() throws Exception {
+
+        product.setPrice(-2.0);
+        productDTO = new ProductDTO(product);
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", existingId)
+                        .header("Authorization", "Bearer " + adminToken)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void updateShouldReturnUnprocessableEntityWhenIdExistsAndAdminLoggedAndPriceIsZero() throws Exception {
+
+        product.setPrice(0.0);
+        productDTO = new ProductDTO(product);
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", existingId)
+                        .header("Authorization", "Bearer " + adminToken)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isUnprocessableEntity());
+    }
 
     @Test
     public void deleteShouldReturnNoContentWhenIdExistsAndAdminLogged() throws Exception {
